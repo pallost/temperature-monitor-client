caa8db922628b34973b8a4036750916cda1366b3
Tamas Pallos <tamas.pallos@vincit.com>
2018-07-09 14:40:29 +0300

    refactor to own function

--- sensor.go
+++ sensor.go
@@ -1,58 +1,71 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
-    "net/http"
-    "bytes"
-    "encoding/json"
+	"fmt"
+	"log"
+	"time"
+	"net/http"
+	"bytes"
+	"encoding/json"
 
-    "github.com/d2r2/go-dht"
+	"github.com/d2r2/go-dht"
 )
 
 type Measurement struct {
-    Temperature float32
-    Humidity float32
-    Date int64
+	Temperature float32
+	Humidity    float32
+	Date        int64
 }
 
+//func getOutsideTemperature() {
+//	const getUrl = "http://data.fmi.fi/fmi-apikey/d95d22a1-1da2-44d4-abfc-4f9ecb889700/wfs?request=getFeature&storedquery_id=fmi::observations::weather::cities::timevaluepair&parameters=Temperature"
+//
+//	_, err := http.Get(postUrl, "application/json", bytes.NewBuffer(jsonValue))
+//	if err != nil {
+//		log.Println(err)
+//	}
+//}
+
 func makeTimestamp() int64 {
-    return time.Now().UnixNano() / int64(time.Millisecond)
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
 func uploadToGCloud(meas Measurement) {
-        const postUrl = "https://guestbook-sample-179506.appspot.com/add"
+	const postUrl = "https://guestbook-sample-179506.appspot.com/add"
 
-        jsonValue, _ := json.Marshal(meas)
+	jsonValue, _ := json.Marshal(meas)
 
-        _, err := http.Post(postUrl, "application/json", bytes.NewBuffer(jsonValue))
-        if err != nil {
-                log.Println(err)
-        }
+	_, err := http.Post(postUrl, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func readAndSend() {
+	// Read DHT11 sensor data from pin 4, retrying 10 times in case of failure.
+	// You may enable "boost GPIO performance" parameter, if your device is old
+	// as Raspberry PI 1 (this will require root privileges). You can switch off
+	// "boost GPIO performance" parameter for old devices, but it may increase
+	// retry attempts. Play with this parameter.
+	sensorType := dht.DHT22
+
+	temperature, humidity, retried, err :=
+		dht.ReadDHTxxWithRetry(sensorType, 4, false, 10)
+	if err != nil {
+		log.Println(err)
+	}
+	// print temperature and humidity
+	fmt.Printf("Sensor = %v: Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		sensorType, temperature, humidity, retried)
+
+	// write to GCloud
+	newMeasurement := Measurement{temperature, humidity, makeTimestamp()}
+	uploadToGCloud(newMeasurement)
 }
 
 func main() {
-    // Read DHT11 sensor data from pin 4, retrying 10 times in case of failure.
-    // You may enable "boost GPIO performance" parameter, if your device is old
-    // as Raspberry PI 1 (this will require root privileges). You can switch off
-    // "boost GPIO performance" parameter for old devices, but it may increase
-    // retry attempts. Play with this parameter.
-    sensorType := dht.DHT22
-    for {
-        temperature, humidity, retried, err :=
-            dht.ReadDHTxxWithRetry(sensorType, 4, false, 10)
-        if err != nil {
-            log.Println(err)
-        }
-        // print temperature and humidity
-        fmt.Printf("Sensor = %v: Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-            sensorType, temperature, humidity, retried)
-
-	// write to GCloud
-        newMeasurement := Measurement{temperature, humidity, makeTimestamp()}
-        uploadToGCloud(newMeasurement)
-
-        time.Sleep(time.Duration(30)*time.Minute)
-    }
+	for {
+		readAndSend()
+		time.Sleep(time.Duration(30) * time.Minute)
+	}
 }
